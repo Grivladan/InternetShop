7cf8afb51fd9ffd57d8c4f2516d6ec858d01ce23
Grivladan <grigorenkovlad1993@gmail.com>
2017-04-11 01:33:10 +0300

    resolve dependencies with ninject

---
+++ DataAccess/Infrastucture/DataAccessModule.cs
@@ -0,0 +1,23 @@
+﻿using Ninject.Modules;
+using DataAccess.Interfaces;
+using DataAccess.Repository;
+using Ninject.Extensions.Factory;
+using InternetShop.DataAccess.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace DataAccess.Infrastucture
+{
+    public class DataAccessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IRepositoryFactory>().ToFactory();
+            Bind<ApplicationDbContext>().ToSelf();
+            Bind<IUserStore<ApplicationUser>>().To<UserStore<ApplicationUser>>();
+            Bind<UserManager<ApplicationUser>>().ToSelf();
+            Bind(typeof(IRepository<>)).To(typeof(Repository<>));
+            Bind<IUnitOfWork>().To<UnitOfWork>();
+        }
+    }
+}
