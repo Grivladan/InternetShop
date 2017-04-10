0f32591da9de6e7b9c6c5bc0ea187eda9d8b5db2
Grivladan <grigorenkovlad1993@gmail.com>
2017-04-10 20:46:55 +0300

    create generic repository

---
+++ DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DataAccess.Interfaces
+{
+    public interface IRepository<T> where T: class, IEntity
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+
+        IQueryable<T> Query { get; }
+    }
+}
---
+++ DataAccess/Repository/Repository.cs
@@ -0,0 +1,93 @@
+﻿using DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class, IEntity 
+    {
+        private readonly ApplicationDbContext _context;
+        private DbSet<T> _entities;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return Query.ToList();
+        }
+
+        public async Task<T> GetAsync(int id)
+        {
+            return await Query.SingleOrDefaultAsync(x => x.Id == id);
+        }
+
+        public T GetById(int id)
+        {
+            return Query.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Create(T item)
+        {
+            try
+            {
+                if (item == null)
+                    throw new ArgumentNullException("item");
+                Entities.Add(item);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Update(T item)
+        {
+            try
+            {
+                if (item == null)
+                {
+                    throw new ArgumentNullException("item");
+                }
+                _context.Entry(item).State = EntityState.Modified;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            try
+            {
+                var entity = GetById(id);
+                if (entity == null)
+                {
+                    throw new ArgumentNullException("id");
+                }
+                Entities.Remove(entity);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        protected IDbSet<T> Entities
+        {
+            get { return _entities ?? (_entities = _context.Set<T>()); }
+        }
+
+        public IQueryable<T> Query
+        {
+            get { return Entities; }
+        }
+
+    }
+}
