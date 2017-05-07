fda3a852492540b955ac9f4d9f4b424832c9c01c
Grivladan <grigorenkovlad1993@gmail.com>
2017-05-07 17:34:53 +0300

    implement reading images for slider

---
+++ InternetShop/Controllers/SliderController.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InternetShop.Controllers
+{
+    public class SliderController : Controller
+    {        
+        public ActionResult GetSliderImages()
+        {
+            string[] filePaths = Directory.GetFiles(Server.MapPath("~/Content/Images/"));
+
+            List<Slider> files = new List<Slider>();
+            foreach (string filePath in filePaths)
+            {
+                string fileName = Path.GetFileName(filePath);
+                files.Add(new Slider
+                {
+                    title = fileName.Split('.')[0].ToString(),
+                    src = "../Content/Images/" + fileName
+                });
+            }
+
+            return View(files);
+        }
+    }
+}
