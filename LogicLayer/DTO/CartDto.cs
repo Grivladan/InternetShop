7ebb3a4058f0ae08081d6d19fc79b8a2784e5b3f
Grivladan <grigorenkovlad1993@gmail.com>
2017-05-13 01:37:57 +0300

    add dto entities

---
+++ LogicLayer/DTO/CartDto.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Entities;
+using System;
+
+namespace LogicLayer.DTO
+{
+    public class CartDto
+    {
+        public string SessionId { get; set; }
+        public int Id { get; set; }
+        public int Count { get; set; }
+        public DateTime Date { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
---
+++ LogicLayer/DTO/OrderDto.cs
@@ -0,0 +1,27 @@
+﻿using DataAccess.Entities;
+using InternetShop.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace LogicLayer.DTO
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Adress { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Total { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+
+        public virtual ApplicationUser Owner { get; set; }
+        public string OwnerId { get; set; }
+        public virtual ICollection<Detail> Details { get; set; }
+    }
+}
