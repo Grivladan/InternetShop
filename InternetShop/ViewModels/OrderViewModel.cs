c5ba0d1a662c30998c1edd8ef05d28a7550ca925
Grivladan <grigorenkovlad1993@gmail.com>
2017-05-17 00:18:24 +0300

    add validation for order

--- InternetShop/ViewModels/OrderViewModel.cs
+++ InternetShop/ViewModels/OrderViewModel.cs
@@ -2,20 +2,47 @@
 using InternetShop.DataAccess.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace InternetShop.ViewModels
 {
     public class OrderViewModel
     {
         public int Id { get; set; }
-        public string Username { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        [StringLength(160)]
         public string FirstName { get; set; }
-        public string SecondName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(160)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(70)]
         public string Adress { get; set; }
+
+        [Required(ErrorMessage = "City is required")]
+        [StringLength(40)]
+        [RegularExpression(@"([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+)", ErrorMessage = "Entered country name is not valid.")]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "Country is required")]
+        [StringLength(40)]
+        [RegularExpression(@"([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+)", ErrorMessage = "Entered country name is not valid.")]
         public string Country { get; set; }
+
+        [Required(ErrorMessage = "Email Address is required")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Email is is not valid.")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"/^[0-9()-]+$/", ErrorMessage = "Entered phone format is not valid.")]
         public string Phone { get; set; }
+
         public DateTime? Date { get; set; }
         public decimal Total { get; set; }
         public OrderStatus OrderStatus { get; set; }
--- LogicLayer/DTO/OrderDto.cs
+++ LogicLayer/DTO/OrderDto.cs
@@ -8,9 +8,8 @@ namespace LogicLayer.DTO
     public class OrderDto
     {
         public int Id { get; set; }
-        public string Username { get; set; }
         public string FirstName { get; set; }
-        public string SecondName { get; set; }
+        public string LastName { get; set; }
         public string Adress { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
